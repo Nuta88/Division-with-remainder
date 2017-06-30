f0e74c8da68aa4fd9771a8ead27798533b76e274
nuta88 <anna.vagner-88@mail.ru>
2017-06-30 22:17:27 +0300

    Added changes.

--- division.js
+++ division.js
@@ -1,37 +1,33 @@
 function getResultDivisionWithRemainder(dividend, divider) {
-	var arrNumDividend = getArrNumbersDividendFromArrStr(String(dividend));
+	var strDividend = String(dividend);
 	var strQuotient = "";
-	var remainder = "";
+	var remainder  = "";
 	var newDividend = 0;
-	for (var i = 0; i < arrNumDividend.length; i++) {
-		newDividend = Number(remainder + arrNumDividend[i]);
-		var numQuotient = Math.floor((newDividend / divider));
-		strQuotient += String(numQuotient);
-		remainder = String(newDividend - (numQuotient * divider));
-		if (arrNumDividend.length >= String(divider).length && strQuotient=== "0") {
-			strQuotient= "";
+	for (var i = 0; i < strDividend.length; i++) {
+		if (strDividend.length < String(divider).length) {
+			return "0[" + String(dividend) + "]";
+		}
+		newDividend = Number(remainder  + Number(strDividend[i]));
+		var privet = Math.floor((newDividend / divider));
+		strQuotient += String(privet);
+		remainder  = String(newDividend - (privet * divider));
+		if (strDividend.length >= String(divider).length && strQuotient === "0") {
+			strQuotient = "";
 		}
 	}
-	return strQuotient+ "[" + remainder + "]";
-}
-
-function getArrNumbersDividendFromArrStr(arrStr) {
-	arrNum = [];
-	for (var i = 0; i < arrStr.length; i++) {
-		arrNum[i] = Number(arrStr[i]);
-	}
-	return arrNum;
+	return strQuotient + "[" + remainder  + "]";
 }
 
 function testingdivisonWithRemainder() {
-	var inputDivider = 12;
-	var inputDividend = 213;
-	var expected = "17[9]";
+	var inputDivider = 128;
+	var inputDividend = 15;
+	var expected = "0[15]";
 	var actual = getResultDivisionWithRemainder(inputDividend, inputDivider);
-	if (actual == expected) {
+	if (actual === expected) {
 		document.write("function is working good" + " " + actual);
 	} else {
 		document.write("function is working bad" + " " + actual);
 	}
 }
+
 testingdivisonWithRemainder()
